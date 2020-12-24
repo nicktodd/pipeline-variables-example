8ee7c28167947871abcf13428788e72e1c363d44
Nick Todd <nick.todd@conygre.com>
2020-12-24 15:28:08 +0000

    Added dynamic parameter generation for CFN

--- build-example.py
+++ build-example.py
@@ -1,15 +1,30 @@
 import os
-
+import json
+import datetime
 
 # access a variable
 input_variable = os.getenv('input_variable')
 print("the input_variable has a value of " + input_variable)
 
-# access GIT commit ID
-# to be added later
 
 # now set a variable
 print("setting an output variable named environment to the value of DEV")
 os.putenv("Environment", "DEV")
+
+# create a file containing the timestamped parameter
+today = datetime.datetime.now()
+dateAsString = today.strftime('%Y%m%d%H%M') 
+
+environment_param = "TEST-"+dateAsString
+
+parameter_file_data = {
+    
+        "Parameters" : {
+            "Environment" : environment_param
+        }
+    
+}
+with open('dynamic_parameters.json', 'w') as outfile:    
+        json.dump(parameter_file_data, outfile)
 
 
