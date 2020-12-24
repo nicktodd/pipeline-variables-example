e406f1da62ba74da7ce7d872d5b6a745a69df57b
Nick Todd <nick.todd@conygre.com>
2020-12-24 12:52:00 +0000

    minor comment change

--- build-example.py
+++ build-example.py
@@ -6,7 +6,7 @@ input_variable = os.getenv('input_variable')
 print("the input_variable has a value of " + input_variable)
 
 # access GIT commit ID
-
+# to be added later
 
 # now set a variable
 print("setting an output variable named environment to the value of DEV")
