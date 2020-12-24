f317ee319730f1af033290cf3d5d1b8ac658ebc4
Nick Todd <nick.todd@conygre.com>
2020-12-24 12:30:29 +0000

    Initial commit

---
+++ build-example.py
@@ -0,0 +1,15 @@
+import os
+
+
+# access a variable
+input_variable = os.getenv('input_variable')
+print("the input_variable has a value of " + input_variable)
+
+# access GIT commit ID
+
+
+# now set a variable
+print("setting an output variable named environment to the value of DEV")
+os.putenv("environment", "DEV")
+
+
